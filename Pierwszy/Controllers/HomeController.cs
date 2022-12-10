db2e32d08b59823e9fd68fe6a533a5f5db4d77b7
Specialist969 <pawel.musial@microsoft.wsei.edu.pl>
2022-12-10 15:34:24 +0100

    Dodaj pliki projektów.

---
+++ Pierwszy/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pierwszy.Models;
+using System.Diagnostics;
+
+namespace Pierwszy.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            ViewBag.Powitanie = "To jest informacja z Kontrolera";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Imie = "Paweł";
+            //ViewBag.Godzina = DateTime.Now.Hour;
+            ViewBag.godzina = DateTime.Now.Hour;
+            ViewBag.info = ViewBag.godzina > 18 ? "Dobry Wierczór" : "Dobry Dzień";
+
+            DaneModel[] osoby =
+            {
+                new DaneModel{Imię="Paweł",Nazwisko="Musiał"},
+                new DaneModel{Imię="Piotr",Nazwisko="Nowak"},
+                new DaneModel{Imię="Adam",Nazwisko="Miłek"}
+            };
+
+            return View(osoby);
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Pierwszy/Models/DaneModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pierwszy.Models
+{
+    public class DaneModel
+    {
+        public int Id { get; set; }
+        public string Imię{ get; set; }
+
+        public string Nazwisko { get; set; }
+    }
+}
